dd2e7cc0e9f22775b53f77a68bb6f05db6b868b4
Matthieu van Haperen <mvhaperen70@gmail.com>
2025-10-30 00:51:33 +0400

    new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta httpEquiv="Permissions-Policy" content="camera=(self)" />
